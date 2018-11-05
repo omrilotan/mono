56e081f2663e662e8e9bec6ad3799705d0542afa
Omri Lotan <omrilotan@users.noreply.github.com>
2018-11-05 14:42:34 +0200

    concurrencies via async execute

---
+++ scripts/foreach.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+process.on('unhandledRejection', error => { throw error; });
+
+const {readdir} = require('fs').promises;
+const {resolve} = require('path');
+const execute = require('../packages/async-execute');
+
+const {argv} = process;
+argv.shift();
+argv.shift();
+
+const [command] = argv;
+
+
+(async() => {
+	const packages = await readdir('packages');
+
+	console.log(`Executing on ${packages.length} packages`);
+
+	console.log(...(
+		await Promise.all(
+			packages
+				.filter(item =>
+					!item.startsWith('.')
+				)
+				.map(pkg =>
+					execute(
+						[
+							'cd',
+							resolve(__dirname, '../', 'packages', pkg),
+							'&&',
+							command,
+						].join(' ')
+					)
+				)
+		)
+	));
+})();
