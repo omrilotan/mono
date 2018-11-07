13ab5a05485e734cf2c91b330077afbeacce1191
TomBFiverr <tom.b@fiverr.com>
2018-11-07 15:41:40 +0200

    Adding assignSet

--- packages/assign/lib/assignSet/index.js
+++ packages/assign/lib/assignSet/index.js
@@ -1,5 +1,3 @@
-const assignable = require('../assignable');
-
 /**
  * Assign members of second array to first
  * @private
@@ -7,10 +5,4 @@ const assignable = require('../assignable');
  * @param {Set} source	The set from which to assign respective values
  * no return value
  */
-module.exports = (target, source) => source.forEach(
-	(item, index) => assignable(target[index]) && assignable(item)
-		?
-		require('../assign')(target[index], item)
-		:
-		target.add(item)
-);
+module.exports = (target, source) => source.forEach(item => target.add(item));
