018668be12c1c8809774811886f4e3d409092ca0
TomBFiverr <tom.b@fiverr.com>
2018-11-08 16:24:08 +0200

    consolidate type matchers logic

--- packages/assign/lib/typeMatchers/index.js
+++ packages/assign/lib/typeMatchers/index.js
@@ -1,4 +1,17 @@
-module.exports = {
-	isMap: require('./isMap'),
-	isSet: require('./isSet'),
-};
+/**
+ * @member {Function} isMap
+ * @member {Function} isSet
+ */
+module.exports = [
+	[Map, 'Map'],
+	[Set, 'Set'],
+].reduce(
+	(accumulator, [constructor, name]) => Object.assign(
+		accumulator,
+		{
+			[`is${name}`]: item => item instanceof constructor || (item && typeof item.toString === 'function' && item.toString() === `[object ${name}]`),
+		}
+	),
+	{}
+);
+
--- packages/assign/lib/typeMatchers/isMap.js
+++
@@ -1,7 +0,0 @@
-/**
- * Checks whether a given argument is of data type Set
- * @param item
- * @returns {boolean}
- */
-
-module.exports = item => item instanceof Map || (item && typeof item.toString === 'function' && item.toString() === '[object Map]');
