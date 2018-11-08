a41c2483ed30fa4ec00c8926db634ccddc464aed
Omri Lotan <omrilotan@users.noreply.github.com>
2018-11-08 12:11:36 +0200

    Test in parallel for CI

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 if [ -z $CI ]; then
 	find . | grep spec.js | grep "$1" | grep -v node_modules | xargs echo "./spec.js $@" | xargs mocha --full-trace --timeout 60000
 else
-	find . | grep spec.js | grep "$1" | grep -v node_modules | xargs echo "./spec.js $@" | xargs mocha --full-trace --timeout 60000 --reporter mocha-junit-reporter --reporter-options mochaFile=./junit/test-results.xml
+	node scripts/test.js
 fi
