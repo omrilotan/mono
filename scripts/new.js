8bb7c3ae0f11e087f422439a7a26d9b57c9847ae
Omri Lotan <omrilotan@users.noreply.github.com>
2018-12-04 23:05:41 +0200

    ✏️ mono new script - support scoped packages

--- scripts/new.js
+++ scripts/new.js
@@ -17,10 +17,12 @@ const {argv} = process;
 argv.shift();
 argv.shift();
 
-const [name] = argv;
+const [arg] = argv;
 
 (async() => {
 	const packages = await readdir(resolve(__dirname, '../', 'packages'));
+	const [name, org] = arg.split('/').reverse();
+	const fullName = org ? [org, name].join('/') : name;
 
 	if (packages.includes(name)) {
 		throw new Error(`Package "${name}" already exists`);
@@ -33,7 +35,7 @@ const [name] = argv;
 
 	files['package.json'] = JSON.stringify(
 		{
-			name,
+			name: fullName,
 			version: '0.0.0',
 			description: '',
 			keywords: [],
@@ -55,7 +57,7 @@ const [name] = argv;
 	files['.npmrc'] = 'package-lock=false\naccess=public';
 	files['.npmignore'] = '.*\n*.log\nspec.js';
 	files['.gitattributes'] = '* text eol=lf';
-	files['README.md'] = `# ${name} [![](https://img.shields.io/npm/v/${name}.svg)](https://www.npmjs.com/package/${name}) [![](https://img.shields.io/badge/source--000000.svg?logo=github&style=social)](https://github.com/omrilotan/mono/tree/master/packages/${name})`;
+	files['README.md'] = `# ${name} [![](https://img.shields.io/npm/v/${fullName}.svg)](https://www.npmjs.com/package/${fullName}) [![](https://img.shields.io/badge/source--000000.svg?logo=github&style=social)](https://github.com/omrilotan/mono/tree/master/packages/${name})`;
 	files['index.js'] = 'module.exports = null';
 
 	Promise.all(
