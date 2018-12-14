d5d87a049323b44bf21e903b7ec694bfc37471e2
Omri Lotan <omrilotan@users.noreply.github.com>
2018-12-14 20:41:16 +0200

    📖 Add description to package pages

--- scripts/doc.sh
+++ scripts/doc.sh
@@ -6,7 +6,7 @@ function search {
 		if test -d "${1}/${FILE}"; then
 			echo -e "\033[1mWriting docs for ${FILE}\033[0m\n"
 			cd "${1}/${FILE}"
-			eval "../../packages/markt/bin/markt --destination ../../docs/${FILE}/index.html --template ../../src/index.html --title ${FILE}"
+			eval "../../packages/markt/bin/markt --destination ../../docs/${FILE}/index.html --template ../../src/index.html --title ${FILE} --description \"$(node -p 'require(`./package.json`).description')\""
 			cd -
 		fi
 	done
