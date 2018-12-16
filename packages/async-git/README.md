a9dd13b2643f77ac605ea3d42af11ec540ecf960
Omri Lotan <omrilotan@users.noreply.github.com>
2018-12-16 23:19:47 +0200

    🔪 async-git reduce code duplication

--- packages/async-git/README.md
+++ packages/async-git/README.md
@@ -1,19 +1,13 @@
 # async-git [![](https://img.shields.io/npm/v/async-git.svg)](https://www.npmjs.com/package/async-git) [![](https://img.shields.io/badge/source--000000.svg?logo=github&style=social)](https://github.com/omrilotan/mono/tree/master/packages/async-git)
 
-Retrieves data from current git repository
+## 👾 Retrieve data from current git repository
+
+> Properties are async (getters)
 
 ```js
 const git = require('async-git');
 
-const [
-	author_name,
-	commit_message,
-] = [
-	await git.author,
-	await git.message,
-];
-
-`${author_name} committed ${commit_message}` // Omri committed Some changes
+`${await git.author} committed ${await git.message}` // Omri committed Some changes
 ```
 
 | Property | Type | Description
--- packages/async-git/index.js
+++ packages/async-git/index.js
@@ -1,35 +1,31 @@
 const exec = require('async-execute');
 
-const formats = {
-	author: 'an',
-	comitter: 'cn',
-	email: 'ae',
-	sha: 'H',
-	short: 'h',
-	subject: 's',
-	message: 'B',
-	body: 'b',
-};
+const formats = [
+	['author'  , 'an'],
+	['comitter', 'cn'],
+	['email'   , 'ae'],
+	['sha'     , 'H' ],
+	['short'   , 'h' ],
+	['subject' , 's' ],
+	['message' , 'B' ],
+	['body'    , 'b' ],
+];
+
+const show = async item => await exec(`git show -s --format=%${item}`);
 
 const getters = Object.assign(
 	{
-		name: async () => await exec('basename -s .git `git config --get remote.origin.url`'),
-		branch: async () => await exec('git rev-parse --abbrev-ref HEAD'),
-		date: async () => new Date(parseInt(await exec('git show -s --format=%at')) * 1000),
+		name: exec.bind(null, 'basename -s .git `git config --get remote.origin.url`'),
+		branch: exec.bind(null, 'git rev-parse --abbrev-ref HEAD'),
+		date: async () => new Date(parseInt(await show('at')) * 1000),
 	},
-	Object.entries(formats).reduce(
-		(props, [key, value]) => Object.assign(
-			props,
-			{
-				[key]: async () => await exec(`git show -s --format=%${value}`),
-			}
-		),
-		{}
+	...formats.map(
+		([key, value]) => ({[key]: show.bind(null, value)})
 	)
 );
 
 /**
- * @typedef     gitGet
+ * @typedef     asyncGit
  * @description Get git info
  * @type     {Object}
  * @property {Promise<String>} name     Project name
@@ -41,9 +37,10 @@ const getters = Object.assign(
  * @property {Promise<String>} short    7 Character Unique identifier of the last commit
  * @property {Promise<String>} subject  Most recent commit subject
  * @property {Promise<String>} message  Most recent commit full message
+ * @property {Promise<String>} body     Most recent commit message body
  */
-module.exports = Object.defineProperties(
-	{},
+Object.defineProperties(
+	module.exports,
 	Object.entries(getters).reduce(
 		(props, [key, value]) => {
 			return Object.assign(
