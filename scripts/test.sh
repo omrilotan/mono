bdc76ce5c149e12d7c2b83d1831b26bec39f92c5
Omri Lotan <omrilotan@users.noreply.github.com>
2018-12-16 23:37:16 +0200

    ✔︎ Remove concurrent tests, use mocha normally

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,1 @@
-if [ -z $CI ]; then
-	find . | grep spec.js | grep "$1" | grep -v node_modules | xargs echo "./spec.js $@" | xargs mocha --full-trace --timeout 60000
-else
-	node scripts/test.js
-fi
+find . | grep spec.js | grep "$1" | grep -v node_modules | xargs echo "./spec.js $@" | xargs mocha --full-trace --timeout 60000
