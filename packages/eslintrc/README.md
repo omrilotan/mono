fc76d44a94e741a54a3b0a08c0c8178ae827b4fc
Omri <omri@hamadgera.com>
2019-03-28 21:43:51 +0200

    🔧 My usual eslintrc

---
+++ packages/eslintrc/README.md
@@ -0,0 +1,16 @@
+# eslintrc [![](https://img.shields.io/npm/v/@omrilotan/eslintrc.svg)](https://www.npmjs.com/package/@omrilotan/eslintrc) [![](https://img.shields.io/badge/source--000000.svg?logo=github&style=social)](https://github.com/omrilotan/mono/tree/master/packages/eslintrc)
+
+## 🔧 My usual eslintrc
+
+```
+npm i -D @omrilotan/eslint-config-rc
+```
+
+.eslintrc
+```
+{
+    "extends": [
+        "@omrilotan/rc"
+    ]
+}
+```
---
+++ packages/eslintrc/index.js
@@ -0,0 +1,41 @@
+module.exports = {
+	extends: 'eslint:recommended',
+	env: {
+		node: true,
+		es6: true,
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	rules: {
+		indent: [
+			'error', 'tab', {
+				SwitchCase: 1,
+				FunctionDeclaration: {
+					body: 1,
+					parameters: 2,
+				},
+			},
+		],
+		semi: ['error', 'always'],
+		'arrow-parens': [2, 'as-needed'],
+		'comma-dangle': ['error', 'always-multiline'],
+		quotes: ['error', 'single'],
+		'dot-location': ['error', 'property'],
+		'dot-notation': 'error',
+		'no-implicit-globals': 'error',
+	},
+	overrides: [
+		{
+			files: [ '**/spec.js' ],
+			env: {
+				mocha: true,
+			},
+			globals: {
+				expect: true,
+				assert: true,
+			},
+		},
+	],
+};
