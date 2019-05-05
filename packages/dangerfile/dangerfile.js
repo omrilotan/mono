eb863ca0b25a5feca660881608f008c7a27e0e7a
Omri Lotan <516342+omrilotan@users.noreply.github.com>
2019-05-05 22:49:45 +0300

    dangerfile 🚨 Centralised Dangerfile (#49)

---
+++ packages/dangerfile/dangerfile.js
@@ -0,0 +1,12 @@
+// const { danger, fail, warn, message } = require('danger'); // Globally imported
+
+const {github: {pr: {body, base}}} = danger;
+
+if (!body || body.length < 2) {
+	fail('Please add a description to your pull request.');
+}
+
+if (base.ref !== 'master') {
+	warn(`The base branch for this PR is \`${base.ref}\`. Are you sure you want to target something other than the \`master\` branch?`);
+}
+
