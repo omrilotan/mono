ea35b3ceb04f58c6c31bfd82a90b01a54556dacb
Omri <omri@hamadgera.com>
2019-05-05 22:04:06 +0300

    bad things happen when you forget about that one little change you made ages ago

--- packages/dangerfile/bin.js
+++ packages/dangerfile/bin.js
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
 
-const { promises: { readFile, writeFile, access }, F_OK } = require('fs');
+const { promises: { readFile, writeFile } } = require('fs');
 const { join } = require('path');
 const execute = require('async-execute');
+const exist = require('@does/exist');
 const FILENAME = 'dangerfile.js';
 
 (async() => {
 	const target = join(process.cwd(), FILENAME);
 
-	if (await access(target, F_OK)) {
-		console.warn('Dangerfile already exists. Using existing file');
+	if (await exist(target)) {
+		console.warn('Using the existing Dangerfile');
 	} else {
+		console.info('Creating a new Dangerfile');
 		const source = join(__dirname, FILENAME);
 		const content = await readFile(source);
 		writeFile(target, content.toString());
