246ee31b03982c18dbe041003e8b7029ae75a88a
Omri <omri@hamadgera.com>
2019-05-05 22:44:10 +0300

    forgot we're not using a smart language

--- packages/dangerfile/dangerfile.js
+++ packages/dangerfile/dangerfile.js
@@ -1,18 +1,12 @@
 // const { danger, fail, warn, message } = require('danger'); // Globally imported
 
 const {github: {pr: {body, base}}} = danger;
-let errors = 0;
 
 if (!body || body.length < 2) {
-	fail('Pull requests need descriptions.');
-	++errors;
+	fail('Please add a description to your pull request.');
 }
 
 if (base.ref !== 'master') {
 	warn(`The base branch for this PR is \`${base.ref}\`. Are you sure you want to target something other than the \`master\` branch?`);
-	++errors;
 }
 
-if (errors) {
-	message('Please address all errors and warnings before merging this pull request');
-}
