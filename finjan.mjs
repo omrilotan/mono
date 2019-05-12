583eff56546e5accc3549e1a730bbb7e1016958c
Omri <omri@hamadgera.com>
2019-05-12 09:24:04 +0300

    ☕️ use finjan for es6 tests

---
+++ finjan.mjs
@@ -0,0 +1,2 @@
+import chai from 'chai';
+Object.assign(global, chai);
--- packages/module-scope/spec.mjs
+++ packages/module-scope/spec.mjs
@@ -3,13 +3,11 @@ import scope from './index.mjs';
 const {__dirname, __filename} = scope(import.meta.url);
 const here = [process.cwd(), 'packages', 'module-scope'].join('/');
 
-export default function() {
-	describe('module-scope', () => {
-		it('finds __dirname', () => {
-			expect(__dirname).to.equal(here);
-		});
-		it('finds __filename', () => {
-			expect(__filename).to.equal([here, 'spec.mjs'].join('/'));
-		});
+describe('module-scope', () => {
+	it('finds __dirname', () => {
+		expect(__dirname).to.equal(here);
 	});
-}
+	it('finds __filename', () => {
+		expect(__filename).to.equal([here, 'spec.mjs'].join('/'));
+	});
+});
