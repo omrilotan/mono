6d30f9fd9ab2fcf8f4f9cc78a3d327a080fd230c
Omri <omri@hamadgera.com>
2019-07-03 12:16:51 +0100

    async-git Get the last modified date of a file

--- packages/async-git/lib/modified/spec.js
+++ packages/async-git/lib/modified/spec.js
@@ -9,9 +9,12 @@ describe('async-git:modified', async() => {
 	});
 
 	it('Should get the date of the last date a certain file was changes', async() => {
-		const date = await modified('./packagess/async-git/index.js');
+		const date = await modified('./packages/async-git/index.js');
 
-		console.log(date);
 		expect(date).to.be.a('date');
+	});
+
+	it('Should throw an error when the file is not there', async() => {
+		await expect(modified('./packagess/async-git/index.js')).to.be.rejectedWith(Error);
 	});
 });
