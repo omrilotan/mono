efcb764c7790744e69cf47178be82672a0a818d6
Omri <omri@hamadgera.com>
2019-10-31 20:28:52 +0200

    🛵 Publish only if this version was not already published

---
+++ packages/publish/bin.sh
@@ -0,0 +1,15 @@
+function publish {
+	local NAME=$(cat package.json | jq -r ".name")
+	local VERSION=$(cat package.json | jq -r ".version")
+
+	echo "Check if ${NAME}@${VERSION} was published"
+	local EXISTS=$(npm view ${NAME}@${VERSION} version)
+
+	if [[ -z $EXISTS ]]; then
+		npm publish
+	else
+		echo "This version was already published"
+		exit 0
+	fi
+}
+publish
