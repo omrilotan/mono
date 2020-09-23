1245a8fd0da5a133c09a8bae471064a5746a298d
omrilotan <omri@hamadgera.com>
2020-09-23 15:46:03 +0300

    rm ecma-webpack-plugin

--- packages/ecma-webpack-plugin/lib/extension/index.js
+++
@@ -1,8 +0,0 @@
-const { extname } = require('path');
-
-/**
- * Get file extension
- * @param  {string} filename
- * @return {string}
- */
-module.exports = filename => extname(filename).replace(/^\.|\?.*/g, '');
