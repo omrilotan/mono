d6ac51dfacdac7eb707c059b84f46bdb7bfeee00
Hakim <4447631+hbouhadji@users.noreply.github.com>
2020-11-22 05:01:48 +0000

    Fix selenium-chrome-clear-cache (#161)
    * Clear browsing data dialog has been moved...
    
    from advancedPage to basicPage
    
    * Keep legacy

--- packages/selenium-chrome-clear-cache/index.js
+++ packages/selenium-chrome-clear-cache/index.js
@@ -46,8 +46,9 @@ function click(type, selector) {
 	function getSlot(name) {
 		const main = document.querySelector('settings-ui').shadowRoot.children.container.children.main;
 		const settings = findTag(main.shadowRoot.children, 'SETTINGS-BASIC-PAGE');
+		const basicPage = settings.shadowRoot.children.basicPage;
 		const advancedPage = settings.shadowRoot.children.advancedPage;
-		const [ page ] = [ ...advancedPage.children ].find(i => i.section === 'privacy').children;
+		const [ page ] = [ ...basicPage.children, ...advancedPage.children ].find(i => i.section === 'privacy').children;
 		const dialog = findTag(page.shadowRoot.children, 'SETTINGS-CLEAR-BROWSING-DATA-DIALOG');
 
 		return dialog.shadowRoot.children.clearBrowsingDataDialog.querySelector(`[slot="${name}"]`);
